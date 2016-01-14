ce9367b167f7891517dba25b82709aea9f66e8d8
tken85 <terrykennair@Terrys-MBP.attlocal.net>
2016-01-13 23:11:55 -0500

    major style updates

---
+++ main.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  $(document).ready(function(){
+    page.init();
+  });
+
+  var page = {
+    init: function(){
+      page.initStyling();
+      page.initEvents();
+    },
+    initStyling: function(){
+
+    },
+    initEvents: function(){
+      // $('#name').on('mouseover', function(){
+      //   $('#name').text('Terry Kennair').fadeIn(2200);
+      // });
+      // $('#name').on('mouseleave', function(){
+      //   $('#name').text('TK').fadeIn(2200);
+      // });
+
+    },
+  };
+}());
